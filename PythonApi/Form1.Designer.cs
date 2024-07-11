c604f427a68148cb93222356d9b741837362128c
Kaue Negrini Ciglioni <kaueciglioni@hotmail.com>
2024-07-11 13:59:51 -0300

    Adicionar arquivos de projeto.

---
+++ PythonApi/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.GetButton = new System.Windows.Forms.Button();
+            this.guidTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.newGuidTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(220, 76);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(152, 92);
+            this.CreateButton.TabIndex = 0;
+            this.CreateButton.Text = "CREATE";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(389, 76);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(152, 92);
+            this.DeleteButton.TabIndex = 1;
+            this.DeleteButton.Text = "DELETE";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(220, 253);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(152, 92);
+            this.UpdateButton.TabIndex = 2;
+            this.UpdateButton.Text = "UPDATE";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // GetButton
+            // 
+            this.GetButton.Location = new System.Drawing.Point(559, 76);
+            this.GetButton.Name = "GetButton";
+            this.GetButton.Size = new System.Drawing.Size(152, 92);
+            this.GetButton.TabIndex = 3;
+            this.GetButton.Text = "GET";
+            this.GetButton.UseVisualStyleBackColor = true;
+            this.GetButton.Click += new System.EventHandler(this.GetButton_Click);
+            // 
+            // guidTextBox
+            // 
+            this.guidTextBox.Location = new System.Drawing.Point(28, 111);
+            this.guidTextBox.Name = "guidTextBox";
+            this.guidTextBox.Size = new System.Drawing.Size(186, 22);
+            this.guidTextBox.TabIndex = 5;
+            this.guidTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "GUID";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(170, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(385, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Insira o valor da GUID e selecione o método que deseja utilizar.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "INSIRA A NOVA GUID";
+            // 
+            // newGuidTextBox
+            // 
+            this.newGuidTextBox.Location = new System.Drawing.Point(28, 288);
+            this.newGuidTextBox.Name = "newGuidTextBox";
+            this.newGuidTextBox.Size = new System.Drawing.Size(186, 22);
+            this.newGuidTextBox.TabIndex = 10;
+            this.newGuidTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(170, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(302, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Insira o valor da nova GUID e clique em UPDATE";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(735, 493);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.newGuidTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guidTextBox);
+            this.Controls.Add(this.GetButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.CreateButton);
+            this.Name = "Form1";
+            this.Text = "v";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button GetButton;
+        private System.Windows.Forms.TextBox guidTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox newGuidTextBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PythonApi/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private string audioGuid = string.Empty;
+        private string newAudioGuid = string.Empty;
+        private string audioFilePath = string.Empty;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void CreateButton_Click(object sender, EventArgs e)
+        {
+            audioGuid = guidTextBox.Text;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Audio files (*.mp3;*.wav)|*.mp3;*.wav|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    audioFilePath = openFileDialog.FileName;
+
+                    string fileName = Path.GetFileName(audioFilePath);
+                    string newFileName = $"{fileName}";
+
+                    using (var formData = new MultipartFormDataContent())
+                    {
+                        formData.Add(new StringContent(audioGuid), "guid");
+
+                        var fileStream = File.OpenRead(audioFilePath);
+                        var streamContent = new StreamContent(fileStream);
+                        streamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
+                        {
+                            Name = "audios",
+                            FileName = newFileName
+                        };
+                        formData.Add(streamContent);
+
+                        try
+                        {
+                            var response = await client.PostAsync("http://localhost:5000/Home/create", formData);
+                            response.EnsureSuccessStatusCode();
+                            var responseString = await response.Content.ReadAsStringAsync();
+                            MessageBox.Show(responseString);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erro ao enviar o arquivo: " + ex.Message);
+                        }
+                        finally
+                        {
+                            fileStream.Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void DeleteButton_Click(object sender, EventArgs e)
+        {
+            audioGuid = guidTextBox.Text;
+            try
+            {
+                var response = await client.DeleteAsync($"http://localhost:5000/Home/delete/{audioGuid}");
+                response.EnsureSuccessStatusCode();
+                var responseString = await response.Content.ReadAsStringAsync();
+                MessageBox.Show(responseString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao deletar o arquivo: " + ex.Message);
+            }
+        }
+
+        private async void UpdateButton_Click(object sender, EventArgs e)
+        {
+            audioGuid = guidTextBox.Text;
+            newAudioGuid = newGuidTextBox.Text;
+
+            try
+            {
+                using (var formData = new MultipartFormDataContent())
+                {
+                    formData.Add(new StringContent(newAudioGuid), "guid");
+
+                    using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                    {
+                        openFileDialog.Filter = "Audio files (*.mp3;*.wav)|*.mp3;*.wav|All files (*.*)|*.*";
+                        if (openFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            audioFilePath = openFileDialog.FileName;
+
+                            string fileName = Path.GetFileName(audioFilePath);
+                            string newFileName = $"{fileName}";
+
+                            var fileStream = File.OpenRead(audioFilePath);
+                            var streamContent = new StreamContent(fileStream);
+                            streamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
+                            {
+                                Name = "audios",
+                                FileName = newFileName
+                            };
+                            formData.Add(streamContent);
+
+                            var response = await client.PutAsync($"http://localhost:5000/Home/update/{audioGuid}", formData);
+                            response.EnsureSuccessStatusCode();
+
+                            var responseString = await response.Content.ReadAsStringAsync();
+                            MessageBox.Show(responseString);
+
+                            fileStream.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao atualizar o arquivo: " + ex.Message);
+            }
+        }
+
+        private async void GetButton_Click(object sender, EventArgs e)
+        {
+            string audioGuid = guidTextBox.Text;
+
+            try
+            {
+                var response = await client.GetAsync($"http://localhost:5000/Home/{audioGuid}");
+                response.EnsureSuccessStatusCode();
+
+                var responseStream = await response.Content.ReadAsStreamAsync();
+
+                string downloadsPath = "\\\\wsl.localhost\\Ubuntu\\home\\kaue\\voxProjects\\api-audios\\downloads";
+
+                if (!Directory.Exists(downloadsPath))
+                {
+                    Directory.CreateDirectory(downloadsPath);
+                }
+
+                string fileName = $"{audioGuid}_downloaded_audio.mp3";
+                string downloadFilePath = Path.Combine(downloadsPath, fileName);
+
+                using (var fileStream = new FileStream(downloadFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    await responseStream.CopyToAsync(fileStream);
+                }
+
+                MessageBox.Show($"Áudio baixado com sucesso em: {downloadFilePath}");
+
+                System.Diagnostics.Process.Start("explorer.exe", downloadsPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao baixar o arquivo: {ex.Message}");
+            }
+        }
+    }
+}
