495371c4ae49900563026f138125c822477ccaee
Kauê Ciglioni <102191780+kaueciglioni@users.noreply.github.com>
2024-07-17 09:52:19 -0300

    Classe para controle dos métodos request da API

---
+++ Log.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using VoxLogger;
+
+namespace WindowsFormsApp
+{
+    /// <summary>
+    /// Classe modelo para a criação e controle do Log.
+    /// Toda vez que for utilizar mais de um arquivo de log por projeto, deve ser criado um log por projeto.
+    /// </summary>
+    public class Log
+    {
+        #region Constant
+        /// <summary>
+        /// Nome do projeto
+        /// Deve ser atribuido no construtor
+        /// </summary>
+        private const string ProjectName = "WindowsFormsApp";
+        #endregion
+
+        #region Constructors / Finalizer
+        /// <summary>
+        /// Utilizado somente para criação da classe
+        /// </summary>
+        /// <param name="p_configLogfileName"></param>
+        private Log(string p_configLogfileName)
+        {
+            try
+            {
+                Logger = new Logger(p_configLogfileName, ProjectName);
+            }
+            catch (Exception l_exception)
+            {
+                Console.WriteLine(l_exception.ToString());
+                throw;
+            }
+        }
+        #endregion
+
+        #region Atributes
+        /// <summary>
+        /// Objeto Logger Private
+        /// </summary>
+        private static Logger m_logger;
+
+        /// <summary>
+        /// Instancia do objeto Singleton
+        /// </summary>
+        private static Log m_instance;
+
+        /// <summary>
+        /// Lock responsável para controle de concorrência na construção do objeto
+        /// </summary>
+        private static readonly object m_lock = new object();
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Controlador do objeto Logger
+        /// </summary>
+        public static Logger Logger
+        {
+            get
+            {
+                try
+                {
+                    if (m_logger == null)
+                    {
+                        string l_configLogfileName = VoxConfigurationManager.VoxConfiguration.GetParameter("configFileLogPath").ToString();
+                        //string l_configLogfileName = "";
+
+                        //Verificar se o Nó configFilelogPath está no App.VoxConfig
+                        if (string.IsNullOrEmpty(l_configLogfileName))
+                        {
+                            CreateInstance(null);
+                        }
+                        else
+                        {
+                            string l_directoryLogName;
+
+                            try
+                            {
+                                l_directoryLogName = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+                            }
+                            catch
+                            {
+                                l_directoryLogName = "";
+                            }
+
+                            if (string.IsNullOrEmpty(l_directoryLogName))
+                            {
+                                l_directoryLogName = AppDomain.CurrentDomain.BaseDirectory;
+                            }
+                            //else
+                            //{
+                            //    l_directoryLogName = Path.GetDirectoryName(l_directoryLogName);
+                            //}
+
+                            if (l_directoryLogName != null)
+                            {
+                                CreateInstance(l_directoryLogName.TrimEnd('\\') + "\\" + l_configLogfileName);
+                            }
+                        }
+                    }
+                    return m_logger;
+                }
+                catch (Exception l_exception)
+                {
+                    throw;
+                }
+            }
+            private set { m_logger = value; }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Retorna a Instancia já criada ou cria
+        /// </summary>
+        /// <param name="p_configLogfileName">Caminho do arquivo de configuração do Log</param>
+        /// <returns>Instancia da classe Singleton</returns>
+        private static Log CreateInstance(string p_configLogfileName)
+        {
+            try
+            {
+                lock (m_lock)
+                {
+                    if (m_instance == null)
+                    {
+                        m_instance = new Log(p_configLogfileName);
+                    }
+                }
+                return m_instance;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
---
+++ RequestApi.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp;
+
+namespace PythonApi
+{
+    public class RequestApi
+    {
+        private static readonly HttpClient m_client = new HttpClient();
+        public string l_local_arquivo = VoxConfigurationManager.VoxConfiguration.GetParameter("link").ToString();
+
+        public async Task<string> CreateAudio(string p_guid, string p_audioFilePath)
+        {
+            string l_responseString = string.Empty;
+
+            string l_fileName = Path.GetFileName(p_audioFilePath);
+            string l_newFileName = $"{l_fileName}";
+
+            using (var l_formData = new MultipartFormDataContent())
+            {
+                l_formData.Add(new StringContent(p_guid), "guid");
+
+                var l_fileStream = File.OpenRead(p_audioFilePath);
+                var l_streamContent = new StreamContent(l_fileStream);
+                l_streamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
+                {
+                    Name = "audios",
+                    FileName = l_newFileName
+                };
+                l_formData.Add(l_streamContent);
+
+                try
+                {
+                    var l_response = await m_client.PostAsync("http://localhost:5000/Home/create", l_formData);
+                    l_response.EnsureSuccessStatusCode();
+                    l_responseString = await l_response.Content.ReadAsStringAsync();
+                    Log.Logger.Debug("RequestApi", "CreateAudioAsync", "Requisição enviada com sucesso.");
+                }
+                catch (Exception l_ex)
+                {
+                    Log.Logger.Error("RequestApi", "CreateAudioAsync", l_ex);
+                    throw new Exception("Erro ao enviar o arquivo: " + l_ex.Message);
+                }
+                finally
+                {
+                    l_fileStream.Close();
+                    Log.Logger.Debug("RequestApi", "CreateAudioAsync", "GUID e AUDIOS CRIADAS COM SUCESSO");
+                }
+            }
+
+            return l_responseString;
+        }
+
+        public async Task<string> DeleteAudio(string p_guid)
+        {
+            string l_responseString = string.Empty;
+
+            try
+            {
+                var l_response = await m_client.DeleteAsync($"http://localhost:5000/Home/delete/{p_guid}");
+                l_response.EnsureSuccessStatusCode();
+                l_responseString = await l_response.Content.ReadAsStringAsync();
+                Log.Logger.Debug("RequestApi", "DeleteAudio", "Arquivo deletado com sucesso");
+            }
+            catch (Exception l_ex)
+            {
+                Log.Logger.Error("RequestApi", "DeleteAudioAsync", l_ex);
+                throw new Exception("Erro ao deletar o arquivo: " + l_ex.Message);
+            }
+
+            return l_responseString;
+        }
+
+        public async Task<string> UpdateAudio(string p_guid, string p_newGuid, string p_audioFilePath)
+        {
+            string l_responseString = string.Empty;
+
+            using (var l_formData = new MultipartFormDataContent())
+            {
+                l_formData.Add(new StringContent(p_newGuid), "guid");
+
+                using (OpenFileDialog l_openFileDialog = new OpenFileDialog())
+                {
+                    l_openFileDialog.Filter = "Audio files (*.mp3;*.wav)|*.mp3;*.wav|All files (*.*)|*.*";
+                    if (l_openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        p_audioFilePath = l_openFileDialog.FileName;
+
+                        string l_fileName = Path.GetFileName(p_audioFilePath);
+                        string l_newFileName = $"{l_fileName}";
+
+                        var l_fileStream = File.OpenRead(p_audioFilePath);
+                        var l_streamContent = new StreamContent(l_fileStream);
+                        l_streamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
+                        {
+                            Name = "audios",
+                            FileName = l_newFileName
+                        };
+                        l_formData.Add(l_streamContent);
+
+                        try
+                        {
+                            var l_response = await m_client.PutAsync($"http://localhost:5000/Home/update/{p_guid}", l_formData);
+                            l_response.EnsureSuccessStatusCode();
+                            l_responseString = await l_response.Content.ReadAsStringAsync();
+                            Log.Logger.Debug("RequestApi", "UpdateAudio", "Arquivo alterado com sucesso");
+
+                            l_fileStream.Close();
+                        }
+                        catch (Exception l_ex)
+                        {
+                            Log.Logger.Error("RequestApi", "UpdateAudio", l_ex);
+                            throw new Exception("Erro ao atualizar o arquivo: " + l_ex.Message);
+                        }
+                    }
+                }
+            }
+
+            return l_responseString;
+        }
+
+        public async Task<string> GetAudio(string p_guid)
+        {
+            string l_downloadFilePath = string.Empty;
+
+            try
+            {
+                var l_response = await m_client.GetAsync($"http://localhost:5000/Home/{p_guid}");
+                l_response.EnsureSuccessStatusCode();
+
+                var l_responseStream = await l_response.Content.ReadAsStreamAsync();
+
+                string l_downloadsPath = l_local_arquivo;
+
+                if (!Directory.Exists(l_downloadsPath))
+                {
+                    Directory.CreateDirectory(l_downloadsPath);
+                }
+
+                string l_fileName = $"{p_guid}_downloaded_audio.mp3";
+                l_downloadFilePath = Path.Combine(l_downloadsPath, l_fileName);
+
+                using (var l_fileStream = new FileStream(l_downloadFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    await l_responseStream.CopyToAsync(l_fileStream);
+                }
+
+                Log.Logger.Debug("RequestApi", "GetAudio", "Áudio baixado com sucesso");
+            }
+            catch (Exception l_ex)
+            {
+                Log.Logger.Error("RequestApi", "GetAudio", l_ex);
+                throw new Exception("Erro ao baixar o arquivo: " + l_ex.Message);
+            }
+
+            return l_downloadFilePath;
+        }
+    }
+}
